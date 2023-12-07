7ac570776c80ee77e09ad8861592dadf9621f8f5
Kacper Kiermasz <kacperkiermasz@gmail.com>
2023-12-07 01:25:40 +0000

    remove unused imports

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,14 +1,10 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { CardActionArea } from "@mui/material";
 import content from "../content.json";
 import ArtPreview from "./ArtPreview";
 
